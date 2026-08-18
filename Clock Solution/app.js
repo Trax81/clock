b4ebe4041bfa633e641143f953da096ff3a477b4
Cara Cottingham <boops@Caras-MacBook-Pro.local>
2026-08-18 15:55:51 -0700

    Travis

--- Clock Solution/app.js
+++ Clock Solution/app.js
@@ -2,7 +2,7 @@
 const secondHandle = document.querySelector('.handle__second')
 const minuteHandle = document.querySelector('.handle__minute')
 const hourHandle = document.querySelector('.handle__hour')
-
+const millisecond = document.querySelector('.handle__millisecond')
 //requestAnimationFrame(setHandles)
 
 setInterval(() => {
@@ -18,6 +18,7 @@ const d = new Date()
 const hours = d.getHours()
 const minutes = d.getMinutes()
 const seconds = d.getSeconds()
+const milliseconds = d.getMilliseconds()
 
 const extraMinutesAngle = seconds * 0.1
 const minutesAngle = minutes * 6 + extraMinutesAngle
@@ -25,10 +26,13 @@ const minutesAngle = minutes * 6 + extraMinutesAngle
 const extraHoursAngle = minutes * 0.5
 const hoursHandle = hours * 30
 
+const extraMillisecondsAngle = minutes * 0.5
+const millisecondsHandle = hours * 30
+
 
 secondHandle.style.transform = `translate(-50%) rotate(${seconds * 6}deg)`
 hourHandle.style.transform = `translate(-50%) rotate(${hoursHandle}deg)`
 minuteHandle.style.transform = `translate(-50%) rotate(${minutesAngle}deg)`
-
+millisecondHandle.style.transform = `translate(-50%) rotate(${millisecondsAngle}deg)`
 //requestAnimationFrame(setHandles)
 }
